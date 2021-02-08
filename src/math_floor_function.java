334a73bbd5a10689e044909ccc5a181298bd97c3
malithk911 <78416064+malithk911@users.noreply.github.com>
2021-02-08 10:15:29 +0530

    pig dice initial commit

--- src/math_floor_function.java
+++
@@ -1,9 +0,0 @@
-public class math_floor_function {
-    public static int dice;
-    public static void main(String [] args) {
-        for (int i = 0; i < 100; i++) {
-            dice = (int) (Math.floor(Math.random() * 6 + 1));
-            System.out.print("\nNumber: " + dice);
-        }
-    }
-}
